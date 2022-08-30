c98f69a7499c24bcacb66eb0625669c4d6d8ec9a
mattzhang31 <104103190+mattzhang31@users.noreply.github.com>
2022-08-29 22:25:31 -0400

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-55956',
+    appId: '1:933474643850:web:3faa680d08bf275d34e273',
+    storageBucket: 'kanbanfire-55956.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCeXrmIu_D9BW_668oSDFDXFPQeoItFYes',
+    authDomain: 'kanbanfire-55956.firebaseapp.com',
+    messagingSenderId: '933474643850',
+  },
+  production: true
+};
